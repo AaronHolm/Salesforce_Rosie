2f760589700c0f938e20a596f14450dd072d40b1
Aaron Holm <aaron.lee.holm@gmail.com>
2019-12-13 08:32:54 -0500

    first commit

--- config.py
+++
@@ -1,8 +0,0 @@
-PARDOT_EMAIL='aholm@seia.org'
-PARDOT_PASS='Sunshine201$'
-PARDOT_USER_KEY='caa507f1827e43c7e27462e933756a4c'
-SF_API_KEY=''
-
-
-SQL_ADDRESS='postgresql://PG:PGAH17@data.seia.org:5432/seia'
-
